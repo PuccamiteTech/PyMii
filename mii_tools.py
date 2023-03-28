517fbccdf55490f74ee8ca4d1ec20ce97af89ac5
PuccamiteTech <53921171+PuccamiteTech@users.noreply.github.com>
2023-03-28 17:11:14 -0400

    Add get_miis_b

--- mii_tools.py
+++ mii_tools.py
@@ -1,7 +1,15 @@
 # By: Sr. Esteban
-# This module defines common PyMii functions.
+# This module defines common PyMii functions and constants.
 
-def get_miis(source, offset, size, padding, empty, limit, prefix):
+WII_DB = "RFL_DB.dat"
+WII_EPL = b"\x00" * 74
+WII_EPA = b"\x00" * 64
+WII_PPA = b"\x00" * 10
+
+WIIU_ODB = "FFL_ODB.dat"
+WIIU_EMA = b"\x00" * 92
+
+def get_miis_a(source, offset, size, padding, empty, limit, prefix):
     try:
         counter = 0
         with open(source, "rb") as infile:
@@ -18,5 +26,17 @@ def get_miis(source, offset, size, padding, empty, limit, prefix):
         return counter
     except FileNotFoundError: print(f"{prefix}: {source} is missing.")
     except PermissionError: print(f"{prefix}: This folder is read-only.")
+
+def get_miis_b(mode): # if-elif-else used for compatibility
+    if mode == 0: # Wii
+        get_miis_a(WII_DB, 0x4, 74, b"", WII_EPL, 100, "WII_PL")
+        get_miis_a(WII_DB, 0x1F1E0, 64, WII_PPA, WII_EPA, 10_000, "WII_PA")
+    elif mode == 1: # 3DS
+        print("3DS not implemented")
+    elif mode == 2: # Wii U
+        get_miis_a(WIIU_ODB, 0x8, 92, b"", WIIU_EMA, 3_000, "WIIU_MA")
+    elif mode == 3: # Switch
+        print("Switch not implemented")
+    else: print(f"Mode {mode} is invalid.")
 
 if __name__ == "__main__": input("This file is a module, not a script.")
